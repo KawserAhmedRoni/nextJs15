d5acc7832a9c9537bef7ebc689fc666887eab0d4
Kawser Ahmed Roni <kawserahmedroni51@gamil.com>
2025-04-09 18:43:05 +0600

    commited by Rony

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+	return (
+		<main>
+			<div className="home-page-area py-5">
+				<div className="container">
+					<h1>I am Home Page</h1>
+				</div>
+			</div>
+		</main>
+	);
+}
